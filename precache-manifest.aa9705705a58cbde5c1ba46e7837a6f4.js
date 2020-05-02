881bf07232646d875727b860966589b0107d7e1c
System Administrator <root@Tevins-MacBook-Air.local>
2020-05-02 09:45:06 -0700

    Updates

---
+++ precache-manifest.aa9705705a58cbde5c1ba46e7837a6f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e7e313d23d5b3c8bae890ce9e0da2b",
+    "url": "/TevinWang/coronasafe/index.html"
+  },
+  {
+    "revision": "a8c4525be717b957eb33",
+    "url": "/TevinWang/coronasafe/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "856029698dcfb1281546",
+    "url": "/TevinWang/coronasafe/static/js/2.28716ce1.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/TevinWang/coronasafe/static/js/2.28716ce1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8c4525be717b957eb33",
+    "url": "/TevinWang/coronasafe/static/js/main.7c02c4e9.chunk.js"
+  },
+  {
+    "revision": "5060ce07bb2224740b7c",
+    "url": "/TevinWang/coronasafe/static/js/runtime-main.5b4de993.js"
+  }
+]);
