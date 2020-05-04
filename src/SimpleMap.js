c80f14cb4a6a8a6cb559775287e9aa30172e3553
TevinWang <43354492+TevinWang@users.noreply.github.com>
2020-05-03 21:55:24 -0700

    Update SimpleMap.js

--- src/SimpleMap.js
+++ src/SimpleMap.js
@@ -1,4 +1,8 @@
+/*
+Full CoronaCrypt Client
 
+Date: May 6
+*/
 
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
@@ -6,24 +10,42 @@ import { geolocated } from "react-geolocated";
 import MyGreatPlace from './MyGreatPlace.js';
 import './App.css';
 import avatar from './img/avatar.svg';
+import ReactPageScroller from 'react-page-scroller';
 import bg from './img/bg.svg';
 import wave from './img/wave.png';
-import coronabottle from './img/coronabottle.png';
+import coronabottle from './img/cclogo.png';
+import FirstComponent from "./FirstComponent";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
-//Sends current coordinates to url
-// eslint-disable-next-line
-function sendCoords(coords, url, username) {
+
+//Sends login username to server
+function sendLogin(url, username) {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.open("POST", url);  
   xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
   xmlhttp.setRequestHeader("Purpose", "login");
+  xmlhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
   xmlhttp.setRequestHeader("Username", username);
+  xmlhttp.send(username);
+	console.log("sent: " + username);
+}
+
+//Sends coordinates of username to server
+function sendCoords(coords, url, username) {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("POST", url);  
+  xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+  xmlhttp.setRequestHeader("Purpose", "sendCoords");
+  xmlhttp.setRequestHeader("Username", username);
+  xmlhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
   xmlhttp.setRequestHeader("Coords", coords.latitude.toString() + ' ' + coords.longitude.toString());
   xmlhttp.send(coords.latitude.toString() + ' ' + coords.longitude.toString());
-  console.log(xmlhttp.responseText);
 	console.log("sent: " + coords.latitude.toString() + ' ' + coords.longitude.toString());
 }
+
 //Formats coordinates for rendering
 function parseCoords(coords) {
   var latlon = coords.slice(1, -1).split(', ');
@@ -34,11 +56,15 @@ function parseCoords(coords) {
     "longitude": lon
   };
 }
+
+//For input animations for login username
 function addcl(){
 	let parent = this.parentNode.parentNode;
 	parent.classList.add("focus");
 }
 
+
+//For input animations for login username
 function remcl(){
 	let parent = this.parentNode.parentNode;
 	if(this.value == ""){
@@ -46,85 +72,68 @@ function remcl(){
 	}
 }
 
+//For reporting a Coronavirus case at current location
 function gotCorona(coords, url, username) {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.open("POST", url);  
   xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
   xmlhttp.setRequestHeader("Purpose", "gotCorona");
   xmlhttp.setRequestHeader("Username", username);
+  xmlhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
   xmlhttp.setRequestHeader("Coords", coords.latitude.toString() + ' ' + coords.longitude.toString());
   xmlhttp.send(coords.latitude.toString() + ' ' + coords.longitude.toString());
   console.log(xmlhttp.responseText);
 	console.log("sent: " + coords.latitude.toString() + ' ' + coords.longitude.toString());
 }
-//Formats coordinates for rendering
-
-function wait(ms){
-  var start = new Date().getTime();
-  var end = start;
-  console.log(end);
-  while(end < start + ms) {
-    end = new Date().getTime();
- }
-}
-//Sends a request to the server in order to get coordinates of every point
-// async function getCoords(coords, url) {
-//   var newcoords;
-//   var xmlHttp = new XMLHttpRequest();
-//     xmlHttp.onreadystatechange = function() { 
-//         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-//             newcoords = xmlHttp.responseText.split("\n");
-//             for (var i=0; i<newcoords.length; i++) {
-//               newcoords[i] = parseCoords(newcoords[i]);
-//             }
-//             SimpleMap.createPoints(url, newcoords);
-//           }
-//       }
-//     xmlHttp.open("GET", "https://RawPythonTest.r2dev2bb8.repl.co", true); 
-//     xmlHttp.setRequestHeader("coords", "37.5042267 -121.9643745");
-//     xmlHttp.send("37.5042267 -121.9643745");
-//     // return newcoords;
-// }
-// Not used -> form posting 
-// xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
 
 class SimpleMap extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: '', loggedIn: false, map: false};
+    this.state = {value: '', loggedIn: false, map: false, currentPage: null};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.toggleMap = this.toggleMap.bind(this);
+    this.corona = this.corona.bind(this);
     this.logOut = this.logOut.bind(this);
-    //sendCoords({latitude:  37.5042267, longitude:  -121.9643745} ,"https://RawPythonTest.r2dev2bb8.repl.co");
     SimpleMap.state = {map: false, latitude: 0.0, longitude: 0.0, coords: [], coordState: false};
-    //this.getCoords({latitude: 37.5042267, longitude: -121.9643745 }, "https://RawPythonTest.r2dev2bb8.repl.co");
   }
+
+  //Handles change in username input
   handleChange(event) {
     this.setState({value: event.target.value});
   }
 
+  //Handles submitting of username input
   handleSubmit(event) {
+    sendLogin("https://RawPythonTest.r2dev2bb8.repl.co", this.state.value);
     sendCoords(this.props.coords ,"https://RawPythonTest.r2dev2bb8.repl.co", this.state.value);
     this.setState({loggedIn: true});
     this.getCoords(this.props.coords, "https://RawPythonTest.r2dev2bb8.repl.co", this.state.value);
     event.preventDefault();
   }
+  
+  //Called by "Report Corona" button, calls gotCorona function
+  corona() {
+    gotCorona(this.props.coords,"https://RawPythonTest.r2dev2bb8.repl.co", this.state.value);
+  }
+  
+  //Called by "Show Map button", sets state of map to show
   toggleMap() {
     this.setState({map: true});
   }
-  // create points on maps
-  setCoordState(coords) {
-    this.setState(coords, coords);
-  }
+
+  //Called by "Logout" button, removes map and username
   logOut() {
     this.setState({loggedIn: false, map: false});
   }
+  
+  //Gets coordinates of covid cases nearby
    getCoords(coords, url, username) {
     console.log(coords);
     var newcoords;
     var xmlHttp = new XMLHttpRequest();
+
       xmlHttp.onreadystatechange = function() {
         
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
@@ -137,27 +146,13 @@ class SimpleMap extends Component {
             SimpleMap.state.coordState = true;
           }
       }
-      // xmlHttp.onreadystatechange = function() { 
-      //     if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-      //         newcoords = xmlHttp.responseText.split("\n");
-      //           for (var i=0; i<newcoords.length; i++) {
-      //             newcoords[i] = parseCoords(newcoords[i]);
-      //             SimpleMap.state.coords.push(newcoords[i]);
-      //           }
-             
-              
-              
-      //       }
-      //   }
-      xmlHttp.open("GET", "https://RawPythonTest.r2dev2bb8.repl.co", true); 
-      //xmlHttp.setRequestHeader("coords", coords.latitude.toString() + " " + coords.longitude.toString());
-      xmlHttp.setRequestHeader("Username", username)
+
+      xmlHttp.open("GET", url, true); 
+      
+      xmlHttp.setRequestHeader("Username", username);
+      xmlHttp.setRequestHeader("Access-Control-Allow-Origin", "*");
+
       xmlHttp.send(null);
-      // if (SimpleMap.state.coords.length > 0) {
-      //   return true;
-      // }
-      // return false;
-      // return newcoords;
   }
   static createPoints(url, coords) {
       var coordsList = coords;
@@ -170,26 +165,11 @@ class SimpleMap extends Component {
       
 
   }
+  //Creates markers on the map
   createTable = () => {
     let table = []
-    // console.log(coordsList);
-    
-    // this.getCoords(this.props.coords, "https://RawPythonTest.r2dev2bb8.repl.co");
-
-    // setTimeout(function() {
-    //   console.log(SimpleMap.state.coords);
-    //   // for (var i = 0; i < 4; i++) {
-    //   //   //Create the parent and add the childrene
-    //   //   // table.push(<MyGreatPlace lat={coordsList.props.lat} lng={coordsList.props.lng} />);
-    //   //   table.push(<MyGreatPlace lat={37.5042267 + i } lng={-121.9643745 + i} />);
-        
-    //   // }
-  
-    // }, 500);
     console.log(SimpleMap.state.coords.length);
     for (var i = 0; i < SimpleMap.state.coords.length; i++) {
-      //Create the parent and add the childrene
-      // table.push(<MyGreatPlace lat={coordsList.props.lat} lng={coordsList.props.lng} />);
       table.push(<MyGreatPlace key={i} lat={SimpleMap.state.coords[i].latitude} lng={SimpleMap.state.coords[i].longitude} />);
       console.log("Coords: Latitude - " + SimpleMap.state.coords[i].latitude.toString() + " Longitude - " + SimpleMap.state.coords[i].longitude.toString());
       
@@ -198,15 +178,6 @@ class SimpleMap extends Component {
     console.log(table);
     
     return table;
-    // console.log(SimpleMap.state.coords);
-    // // Outer loop to create parent
-    // for (var i = 0; i < 4; i++) {
-    //   //Create the parent and add the childrene
-    //   // table.push(<MyGreatPlace lat={coordsList.props.lat} lng={coordsList.props.lng} />);
-    //   table.push(<MyGreatPlace lat={37.5042267 + i } lng={-121.9643745 + i} />);
-      
-    // }
-    // return table
   }
 
   static defaultProps = {
@@ -216,40 +187,57 @@ class SimpleMap extends Component {
     },
     zoom: 11
   };
+
+  //Updates input container
+  componentDidUpdate() {
+    document.querySelectorAll(".input").forEach(input => {
+      input.addEventListener("focus", addcl);
+      input.addEventListener("blur", remcl);
+    });
+  }
   
+  //Complete UI
   render() {
-    // testing code
-    // Geolocation.getCurrentPosition(
-    //   position => {
-    //     this.setState({latitude: position.coords.latitude, longitude: position.coords.longitude});
-    //     sendCoords(this.state, "https://RawPythonTest.r2dev2bb8.repl.co");
-    //   })
-    // this.setState({latitude: this.props.latitude, longitude: this.props.longitude});
-    // if (this.props.coords)  {
-    //   sendCoords(this.props.coords, "https://RawPythonTest.r2dev2bb8.repl.co");
-    //   console.log(getCoords(this.prop]
    
     return !this.props.isGeolocationAvailable ? (
-      <div>Your browser does not support Geolocation</div>
-  ) : !this.props.isGeolocationEnabled ? (
-      <div>Geolocation is not enabled</div>
-  ) : this.props.coords && !this.state.loggedIn ? (
-      <div style={{ height: '100vh', width: '100%', position: 'relative', }}>
+      <div style={{ height: '100vh', width: '100%', position: 'relative', textAlign: 'center' ,display: 'flex'}}>
       <img class="wave" src={wave} />
-      <h2><br></br></h2>
       <div style={{textAlign: 'center', display: 'flex', justifyContent: 'center'}}>
-        <h2><br></br></h2>
-        <img src={coronabottle} style={{height: '50px'}} /><h1>CORONASAFE.</h1>
+      <h2><br></br></h2>
+      <img src={coronabottle} style={{height: '50px'}} /><h1>  CORONA</h1><h1 style={{color: '#25ac7d'}}>CRYPT</h1>
+    </div>
+      <h2>Your browser does not support Geolocation
+      </h2>
       </div>
-      <div class="container">
-		{/* <div class="img">
+  ) : !this.props.isGeolocationEnabled ? (
+    <div style={{ height: '100vh', width: '100%', position: 'relative', textAlign: 'center', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+      <img class="wave" src={wave} />
+      <div style={{textAlign: 'center', display: 'flex', justifyContent: 'center'}}>
+      <h2><br></br></h2>
+      <img src={coronabottle} style={{height: '50px'}} /><h1>  CORONA</h1><h1 style={{color: '#25ac7d'}}>CRYPT</h1>
+    </div>
+      <h2>Geolocation is loading...</h2>
+    </div>
+  ) : this.props.coords && !this.state.loggedIn ? (
+      <div style={{ height: '100vh', width: '100%', position: 'absolute' }}>
+      <img class="wave" src={wave} style={{height: '100vh', position: 'fixed'}} />
+      <h2><br></br></h2>
+      <div style={{textAlign: 'center', display: 'flex', justifyContent: 'center', position: 'fixed', width: '100vw'}}>
+        <h2><br></br></h2>
+        <img src={coronabottle} style={{height: '50px'}} /><h1>  CORONA</h1><h1 style={{color: '#25ac7d'}}>CRYPT</h1>
+      </div>
       
-			<img src={coronabottle} />
-		</div> */}
-    {document.querySelectorAll(".input").forEach(input => {
-    input.addEventListener("focus", addcl);
-    input.addEventListener("blur", remcl);
-  })}
+    <div class="container">
+    <div class="img">
+        <h1>Privacy x Health.</h1>
+        <hr></hr>
+        <li>See cases near you</li>
+        <hr></hr>
+        <li>Report to help others</li>
+        <hr></hr>
+        <li>See a detailed US heatmap</li>
+      </div>
+    
 		<div class="login-content">
 			<form onSubmit={this.handleSubmit}>
 				<img src={avatar} />
@@ -259,7 +247,8 @@ class SimpleMap extends Component {
            		   		<i class="fas fa-user"></i>
            		   </div>
            		   <div class="div">
-           		   		<h5>Username</h5>
+           		   		<h5>Enter a username</h5>
+                    <h5><br></br></h5>
            		   		<input type="text" class="input" value={this.state.value} onChange={this.handleChange} />
            		   </div>
            		</div>
@@ -267,8 +256,8 @@ class SimpleMap extends Component {
             </form>
         </div>
         
-        
     </div>
+    
       </div>
       
   ) : this.props.coords && this.state.loggedIn && !this.state.map ? (
@@ -277,13 +266,18 @@ class SimpleMap extends Component {
     <h2><br></br></h2>
     <div style={{textAlign: 'center', display: 'flex', justifyContent: 'center'}}>
       <h2><br></br></h2>
-      <img src={coronabottle} style={{height: '50px'}} /><h1>CORONASAFE.</h1>
+      <img src={coronabottle} style={{height: '50px'}} /><h1>  CORONA</h1><h1 style={{color: '#25ac7d'}}>CRYPT</h1>
     </div>
     <div class="container">
-  {/* <div class="img">
-    
-    <img src={coronabottle} />
-  </div> */}
+    <div class="img">
+        <h1>Privacy x Health.</h1>
+        <hr></hr>
+        <li>See cases near you</li>
+        <hr></hr>
+        <li>Report to help others</li>
+        <hr></hr>
+        <li>See a detailed US heatmap</li>
+      </div>
   <div class="login-content">
     <form onSubmit={this.handleSubmit}>
       <img src={avatar} />
@@ -304,13 +298,13 @@ class SimpleMap extends Component {
     <h2><br></br></h2>
     <div style={{textAlign: 'center', display: 'flex', justifyContent: 'center'}}>
       <h2><br></br></h2>
-      <img src={coronabottle} style={{height: '50px'}} /><h1>CORONASAFE.</h1>
+      <img src={coronabottle} style={{height: '50px'}} /><h1>  CORONA</h1><h1 style={{color: '#25ac7d'}}>CRYPT</h1>
     </div>
     <div style={{textAlign: 'center'}}>
     <h1><br></br></h1>
     <h1>Welcome, {this.state.value}.</h1>
     <div style={{display: 'flex'}}>
-    <button class="btn" style={{width: '200px', marginLeft: 'auto', marginRight: 'auto'}} onClick={gotCorona(this.props.coords,"https://RawPythonTest.r2dev2bb8.repl.co", this.state.value)}>Report Corona</button>
+    <button class="btn" style={{width: '200px', marginLeft: 'auto', marginRight: 'auto'}} onClick={this.corona}>Report Corona</button>
     <button class="btn" style={{width: '200px', marginLeft: 'auto', marginRight: 'auto'}} onClick={this.logOut}>Logout</button>
      </div>
      </div>
@@ -320,20 +314,18 @@ class SimpleMap extends Component {
           defaultZoom={14}
         >
        {this.createTable()}
-       {/* <MyGreatPlace lat={this.props.coords.latitude} lng={this.props.coords.longitude} />  */}
-          {/* {this.createTable(SimpleMap.state.coords)} */}
-          {/* {this.getCoords(this.props.coords, "https://RawPythonTest.r2dev2bb8.repl.co")} */}
-          {/* {this.getCoords(this.props.coords, "https://RawPythonTest.r2dev2bb8.repl.co")} */}
-          {/* {this.createTable()} */}
-          {/* <AnyReactComponent
-            lat={this.state.latitude}
-            lng={this.state.longitude}
-            text="My Marker"
-          />  */}
+
       </GoogleMapReact>
     </div>
   ) : (
-        <div>Getting the location data&hellip; </div>
+    <div style={{ height: '100vh', width: '100%', position: 'relative', textAlign: 'center'}}>
+      <img class="wave" src={wave} />
+      <div style={{textAlign: 'center', display: 'flex', justifyContent: 'center'}}>
+      <h2><br></br></h2>
+      <img src={coronabottle} style={{height: '50px'}} /><h1>  CORONA</h1><h1 style={{color: '#25ac7d'}}>CRYPT</h1>
+    </div>
+      <h2>Getting location data...</h2>
+    </div>
     );
   }
 }
